bf72b8cc9098378192834a653a873248d2510b73
Liu Xiaoyi <circuitcoder0@gmail.com>
2016-03-04 19:30:32 +0800

    CORS

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var dbc = require('./db/schema');
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var routes = require('./route/base');
 
 var strategy = require('./auth.js');
@@ -12,6 +13,7 @@ passport.use(strategy);
 
 var app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use(routes);
 app.use(function(err, req, res, next) {
   console.error(err.stack);
